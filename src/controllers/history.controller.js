3ad187bf5d0d46d306d3d86f1257191063aa24b8
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-05-27 20:28:37 +0700

    new api

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -11,7 +11,7 @@ const getHistoryById = async (req, res) => {
         //decode token
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         console.log(decoded)
-        var id = decoded.msv
+        var id = decoded.id
         const data = await getDetectionHistory(id);
         if (data === null) {
             const response = {
