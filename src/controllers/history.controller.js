e1f3946966bc39fab1a117587322243f144bdb45
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-05-27 20:24:41 +0700

    new api

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -5,28 +5,39 @@ require('dotenv').config()
 
 const getHistoryById = async (req, res) => {
     //get access token:
-    const token = req.headers['access-token'];
-    console.log(token)
-    //decode token
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    var id = decoded.msv
-    const data = await getDetectionHistory(id);
-    if (data === null) {
-        const response = {
-            code: 0,
-            status: 500,
-            message: "Internal Server Error"
+    try {
+        const token = req.headers['access-token'];
+        console.log(token)
+        //decode token
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        console.log(decoded)
+        var id = decoded.msv
+        const data = await getDetectionHistory(id);
+        if (data === null) {
+            const response = {
+                code: 0,
+                status: 500,
+                message: "Internal Server Error"
+            }
+            res.status(500).json(response);
         }
-        res.status(500).json(response);
+        else {
+            const response = {
+                code: 0,
+                status: 200,
+                message: "ok",
+                data: data
+            }
+            res.status(200).json(response);
+        }
     }
-    else {
+    catch (e) {
         const response = {
             code: 0,
-            status: 200,
-            message: "ok",
-            data: data
+            status: 403,
+            message: "Invalid token"
         }
-        res.status(200).json(response);
+        res.status(403).json(response);
     }
 }
 
