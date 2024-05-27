bb45970d050593d9e4dff72a68be61667a154813
hoanganhvu271 <hoanganhvu271103@gmail.com>
2024-05-27 20:01:31 +0700

    new api

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -1,4 +1,8 @@
 const { getDetectionHistory, insertHistory } = require('../services/history.service')
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
 const getHistoryById = async (req, res) => {
     const id = req.query.id;
     const data = await getDetectionHistory(id);
@@ -23,6 +27,13 @@ const getHistoryById = async (req, res) => {
 
 const postHistory = (req, res) => {
     const data = req.body;
+
+    //get access token:
+    const token = req.headers['access-token'];
+    //decode token
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    data.UserId = decoded.msv
+    console.log(data)
     const result = insertHistory(data);
     if (result === false) {
         const response = {
